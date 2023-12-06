a8db1f6c191c9f9344b504a3c4b357fe48be3f0e
RomanEvgenovichLoboda <loboda.rom@gmail.com>
2023-12-06 16:14:35 +0200

    try runnable

---
+++ src/Main.java
@@ -0,0 +1,75 @@
+//Создайте два текстовых файла с именами, например, countries.txt и capitals.txt. В первом файле должны быть перечислены названия стран,
+//во втором — названия их столиц в той очередности, в которой в первом файле перечислены страны. Например:
+//        countries.txt
+//        Austria China Brazil Netherlands Ukraine Canada
+//        capitals.txt
+//        Vienna Beijing Brasilia Amsterdam Kyiv Ottawa
+//Не поленитесь и занесите в файлы не менее десяти значений. Разделитель выберите по своему усмотрению, можете даже заносить каждое значение в отдельной строке.
+//Теперь вам надо создать приложение, в котором будет два отдельных потока для чтения каждого из файлов. Причем потоки должны работать таким образом. Первый поток читает название
+//первой страны из своего файла и останавливается, передав второму потоку эстафету чтения. Второй поток читает название очередной столицы. Когда прочитано и название страны и название ее столицы,
+//приложение должно создать объект класса CountryInfo.
+//        Class CountryInfo
+//        {
+//            private String country;
+//            private String capital;
+//            . . .
+//        }
+//созданный объект добавляется в коллекцию. Таким образом должны быть прочитаны оба файла и создана коллекция объектов CountryInfo.
+//После этого коллекция должна быть отсортирована по полю country и выведена на экран. Затем коллекция должна быть отсортирована по полю capital и тоже выведена на экран.
+//Потоки ввода-вывода, вид коллекции, способы синхронизации потоков и способ сортировки выберите самостоятельно. Использование лямбда-выражений и анонимных классов приветствуется.
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        writeFiles();
+        MyRunnable runCountries = new MyRunnable("countries.txt");
+        MyRunnable runCapitals = new MyRunnable("capitals.txt");
+        Thread threadCountries = new Thread(runCountries);
+        Thread threadCapitals = new Thread(runCapitals);
+        threadCountries.setDaemon(true);
+        threadCapitals.setDaemon(true);
+        threadCountries.start();
+        threadCapitals.start();
+        try{
+            threadCountries.join();
+            threadCapitals.join();
+
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(ex.getMessage());
+        }
+        System.out.println("ArrayList :");
+        MyRunnable.countryInfos.forEach(System.out::println);
+    }
+
+    public static void writeFiles() {
+        if (!Files.exists(Paths.get("countries.txt"))) {
+            String countries = "Austria\nChina\nBrazil\nNetherlands\nUkraine\nCanada\nNepal\nBangladesh\nPakistan\nMaldives";
+            try (FileWriter fileWriter = new FileWriter("countries.txt", false)) {
+                fileWriter.write(countries);
+                fileWriter.flush();
+                System.out.println("Writed countries.txt");
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        if (!Files.exists(Paths.get("capitals.txt"))) {
+            String capitals = "Vienna\nBeijing\nBrasilia\nAmsterdam\nKyiv\nOttawa\nKathmandu\nDhaka\nIslamabad\nMale";
+            try (FileWriter fileWriter = new FileWriter("capitals.txt", false)) {
+                fileWriter.write(capitals);
+                fileWriter.flush();
+                System.out.println("Writed capitals.txt");
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+}
---
+++ src/MyRunnable.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MyRunnable implements Runnable {
+    static CountryInfo countryInfo;
+    public static ArrayList<CountryInfo> countryInfos;
+    public ArrayList<CountryInfo> getCountryInfos(){
+        return countryInfos;
+    }
+    String path;
+
+    public MyRunnable(String path) {
+        this.path = path;
+        if (countryInfo == null) countryInfo = new CountryInfo();
+        if (countryInfos == null) countryInfos = new ArrayList<>();
+    }
+    @Override
+    public void run(){
+        try(FileReader fileReader = new FileReader(path)) {
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                Thread.sleep(500);
+                if (path.equals("countries.txt")) {
+                    System.out.print(line + " --> ");
+                    synchronized (countryInfo){
+                        countryInfo.setCountry(line);
+                        countryInfo.notify();
+                        countryInfo.wait();
+                    }
+                } else {
+                    System.out.print(line + "\n");
+                    synchronized (countryInfo){
+                        countryInfo.setCapital(line);
+                        countryInfos.add(new CountryInfo(countryInfo));
+                        countryInfo.notify();
+                        countryInfo.wait();
+                    }
+                }
+            }
+        } catch (IOException | InterruptedException e ) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
